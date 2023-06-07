d52e5a13b771f10ed0da67d07a4699c5e1f7dd76
unknown <tothebrenda@gmail.com>
2023-06-07 19:37:49 +0900

    230607

---
+++ 20230607_21.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+list1 = ListNode()
+
+
+print(list1)
+print(list1.val)
+print(list1.next)
+
+list1(2, 3)
+
+print(list1)
+print(list1.val)
+print(list1.next)
+
