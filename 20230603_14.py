6db5ea2cb829790a1e355956921432c8f66d1d74
unknown <tothebrenda@gmail.com>
2023-06-07 19:52:14 +0900

    230607

--- 20230603_14.py
+++ 20230603_14.py
@@ -6,24 +6,20 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-
-        if len(strs) == 1 :
+        ln = len(strs)
+        if ln == 1 :
             return strs[0]
         
-        lst1 = [(st, len(st)) for st in strs]
-        lst2 = sorted(lst1, key=itemgetter(1))
-        lst3 = [i[0] for i in lst2]
-        ln = len(lst3)
-
-        lter = lst3[0]
-        keyln = len(lter)
+        strs.sort(key=len)
+        lter = strs[0]
+        keyln = len(strs[0])
         ans = ""
 
         for i in range(keyln) :
             pr = lter[0:i+1]
 
             for j in range(1, ln) :
-                if lst3[j][0:i+1] == pr :
+                if strs[j][0:i+1] == pr :
                     if j == ln-1 : ans = pr
                     else : continue
                 else : break
--- 20230607_21.py
+++ 20230607_21.py
@@ -1,19 +1,43 @@
+## https://leetcode.com/problems/merge-two-sorted-lists/
+
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-list1 = ListNode()
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
 
+        lst = []
 
-print(list1)
-print(list1.val)
-print(list1.next)
+        while list1 :
+            lst.append(list1.val)
+            list1 = list1.next
 
-list1(2, 3)
+        while list2 :
+            lst.append(list2.val)
+            list2 = list2.next
 
-print(list1)
-print(list1.val)
-print(list1.next)
+        ln = len(lst)
+        lst.sort()
 
+        if ln == 0 : return list1
+        elif ln == 1 :
+            return ListNode(lst[0])
+        else :
+            self.val = ListNode(lst[0])
+            for i in range(1, ln) :
+                self.val = self.addnode(lst[i])
+            return self.val
+
+    def addnode(self, value) :
+        node = self.val
+        while node.next : node = node.next
+        node.next = ListNode(value)
+        return self.val    
