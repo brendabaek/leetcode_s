341331037e0f28965ff36b1c401f5b5958b80b91
unknown <tothebrenda@gmail.com>
2023-06-20 15:02:29 +0900

    230620

--- 20230620_94.py
+++ 20230620_94.py
@@ -1,9 +1,24 @@
-a = []
-if a == True :
-    print("true'")
-if a != True :
-    print("True2")
-if a == False :
-    print("33")
-if a != False :
-    print("44")
+## https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """        
+
+        if root == None : return root
+        ans = []
+        return self.add_values(root, ans)
+        
+    def add_values(self, root, ans) :
+        if root.left != None : self.add_values(root.left, ans)
+        ans.append(root.val)
+        if root.right != None : self.add_values(root.right, ans)
+        return ans
