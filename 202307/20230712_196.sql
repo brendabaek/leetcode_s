8ab5079942cfa0582207328645d76a35c2e590f4
unknown <tothebrenda@gmail.com>
2023-07-12 14:33:41 +0900

    　

---
+++ 202307/20230712_196.sql
@@ -0,0 +1,12 @@
+## https://leetcode.com/problems/delete-duplicate-emails/
+
+# Write your MySQL query statement below
+
+delete
+  from Person
+  where id not in
+    ( select min_id
+      from
+      ( select min(c.id) as min_id
+        from Person as c
+        group by c.email) tmp) ;
