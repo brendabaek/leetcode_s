758e34061b5797b87246ee623c46daf76c2f1f62
Brenda <80332643+totheBrenda@users.noreply.github.com>
2023-08-23 15:13:09 +0900

    Create 20230823_596.sql

---
+++ 202308/easy/20230823_596.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/classes-more-than-5-students/submissions/
+
+-- # Write your MySQL query statement below
+
+select class
+from Courses
+group by class
+having count(student) >= 5;
