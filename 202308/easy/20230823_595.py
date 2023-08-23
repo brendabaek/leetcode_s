da683166c1a89d1bf35aa60f56aa38c2fe9d63f3
Brenda <80332643+totheBrenda@users.noreply.github.com>
2023-08-23 15:02:37 +0900

    Create 20230823_595.py

---
+++ 202308/easy/20230823_595.py
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/big-countries/submissions/
+
+-- # Write your MySQL query statement below
+
+select name, population, area
+from World
+where population >= 25000000 or area >= 3000000;
