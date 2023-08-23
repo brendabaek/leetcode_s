372b81b35e281aaed0cf4c34a221695156578bd8
Brenda <80332643+totheBrenda@users.noreply.github.com>
2023-08-23 15:46:24 +0900

    Create 20230823_50.py

---
+++ 202308/medium/20230823_50.py
@@ -0,0 +1,10 @@
+## https://leetcode.com/problems/powx-n/description/
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x ** n
