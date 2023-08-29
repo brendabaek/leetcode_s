1d81e00d03b132c40ac3538aca168d4c6ac9dad6
unknown <tothebrenda@gmail.com>
2023-08-29 15:05:00 +0900

    　

---
+++ 202308/easy/20230829_653.py
@@ -0,0 +1,32 @@
+## https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        if not root : return
+        lst = self.make_lst(root, [])
+        lst.sort()
+        
+        for i in range(len(lst)-1) :
+            for j in range(len(lst)-1, i, -1) :
+                if lst[i] + lst[j] == k : return True
+                elif lst[i] + lst[j] < k : break
+        return False
+        
+    def make_lst(self, root, lst) :
+        lst.append(root.val)
+        if root.left :
+            lst = self.make_lst(root.left, lst)
+        if root.right :
+            lst = self.make_lst(root.right, lst)
+        return lst
---
+++ 202308/easy/20230829_657.py
@@ -0,0 +1,13 @@
+## https://leetcode.com/problems/robot-return-to-origin/description/
+
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        dics = {'R':0, 'L':0, 'U':0, 'D':0}
+        for m in moves : dics[m] += 1
+
+        if dics['R'] - dics['L'] == 0 and dics['U'] - dics['D'] == 0 : return True
+        else : return False
