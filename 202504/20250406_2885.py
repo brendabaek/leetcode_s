ae5a8b84b27d8af2537a022945fc6b51a3e9047f
Brenda <tothebrenda@gmail.com>
2025-04-06 19:38:52 -0400

    　

---
+++ 202504/20250406_2885.py
@@ -0,0 +1,7 @@
+## https://leetcode.com/problems/rename-columns/
+
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.columns = ['student_id', 'first_name', 'last_name', 'age_in_years']
+    return students
