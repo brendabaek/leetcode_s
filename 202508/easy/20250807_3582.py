8775aa16454cac21db82413ce9f7ccb40dd61c1e
unknown <tothebrenda@gmail.com>
2025-08-07 22:21:48 -0400

    　

---
+++ 202508/easy/20250807_3582.py
@@ -0,0 +1,7 @@
+## https://leetcode.com/problems/generate-tag-for-video-caption/description/
+
+class Solution:
+    def generateTag(self, caption: str) -> str:
+        lst = caption.title().split()
+        if lst == []: return "#"
+        return ("#" + lst[0].lower() + "".join(lst[1:]))[:100]
