85b74ce657412fbaf2fdc0a4fe516bf62525cd5a
unknown <tothebrenda@gmail.com>
2025-09-26 19:04:02 -0400

    　

---
+++ 202509/medium/20250926_1070.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/product-sales-analysis-iii/
+
+-- # Write your MySQL query statement below
+
+select product_id, year as first_year, quantity, price
+from Sales
+where (product_id, year) in (select product_id, min(year) from Sales group by product_id);
