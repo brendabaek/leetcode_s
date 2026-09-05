5fcb6ecf2dd475edce8f48182b9036afa9d308b8
unknown <tothebrenda@gmail.com>
2026-09-05 17:49:53 +0900

    　

---
+++ 202609/20260905_3894.py
@@ -0,0 +1,5 @@
+## https://leetcode.com/problems/traffic-signal-color/
+
+class Solution:
+    def trafficSignal(self, timer: int) -> str:
+        return "Green" if timer == 0 else "Orange" if timer == 30 else "Red" if 30 < timer <= 90 else "Invalid"
